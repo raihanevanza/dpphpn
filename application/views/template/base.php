165d71a8a2e3e32ddaf424552917b15a3191730e
Raihan Evanza <49389186+raihanevanza@users.noreply.github.com>
2022-06-27 23:33:37 +0700

    arinal ganteng

---
+++ application/views/template/base.php
@@ -0,0 +1,36 @@
+
+<!-- semua template -->
+<html>
+<!-- header -->
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cashier</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/css/bootstrap-4.3.1.css') ?>">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/css/custome-style.css') ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/css/style.css') ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/css/table.css') ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/css/sb-admin-2.min.css') ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/css/dataTables.bootstrap4.min.css') ?>">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"></script>
+    </head>
+<!-- header -->
+</html>
+<!-- body -->
+<body>
+    <nav></nav>
+	<?php $this->load->view($page) ?>
+</body>
+<!-- body -->
+<!-- jquery -->
+  <script>
+      src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+      crossorigin="anonymous"
+    </script>
+<!-- css -->
+<!-- boostrap -->
